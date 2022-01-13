291a7ac3642f25e7e68a86c2bfcae5c1d3521099
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-14 02:05:36 +0900

    Completed

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,34 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    #The sql data that is returned are arranged in [id, name, album] ex. [1, "Billie Jean", "Thriller"]
+    #and turn it into a Song instance.
+    #self.new is the same as Song.new in this instance example
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM songs
+    SQL
+
+    #By mapping the execute, it convert each row into Song instances created from the method in line 52
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+    #the first above is making sure to grab the first result in the array
+  end
 end
